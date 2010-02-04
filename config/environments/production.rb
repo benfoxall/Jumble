b388c74a9f4a404fe007673904d9d82e8729c50d
Ben Foxall <benfoxall@gmail.com>
2010-02-04 23:19:00 +0000

    added emial login to production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,17 @@ config.action_view.cache_template_loading            = true
 # config.action_controller.asset_host = "http://assets.example.com"
 
 # Disable delivery errors, bad email addresses will be ignored
-# config.action_mailer.raise_delivery_errors = false
+config.action_mailer.raise_delivery_errors = true
 
 # Enable threaded mode
 # config.threadsafe!
+
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+   :address => "mail.bfoxall.com",
+   :port => 25,
+   :domain => "bfoxall.com",
+   :authentication => :login,
+   :user_name => "jumble@bfoxall.com",
+   :password => "b[1P8M+C",
+}
