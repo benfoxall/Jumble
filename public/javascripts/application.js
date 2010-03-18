71a0347ba40faa9efd9e397ca81ecb1ba4d8d614
Ben Foxall <benfoxall@gmail.com>
2010-03-18 12:21:09 +0000

    added pie charts to the display

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -38,3 +38,45 @@ document.observe('dom:loaded',function(){
 		}})
 	}
 })
+
+
+Pie = Class.create({
+	initialize:function(canvas,data){
+		this.canvas = $(canvas);
+		this.dims   = this.canvas.getDimensions();
+		this.radius = Math.min(this.dims.width,this.dims.height)/2;
+		this.update(data)
+	},
+	translate:function(){
+		// This will do the initial translation,  and save it
+		// (when I get my hands on the canvas documentation)
+	},
+	update:function(data){
+		c = this.canvas.getContext('2d');
+		
+		c.fillStyle = '#fff'
+		c.fillRect(0,0,this.dims.width,this.dims.height);
+		
+		// move to the centre,  and round a bit so that the first slice is at the bottom
+		c.translate(this.dims.width/2,this.dims.height/2);
+		tr = (Math.PI/2)-(Math.PI*data[0]);
+		c.rotate(tr);
+		
+		
+		
+		current = 0;
+		for (var i=0; i < data.length; i++) {
+			// tr += current;
+			c.beginPath();c.moveTo(0,0);
+			c.arc(0,0,this.radius,current,current+=(data[i]*Math.PI*2),false);
+			c.fillStyle = this.sliceColour(i/data.length);c.fill();
+		};
+		
+		//move back (instead of pop for now)
+		c.rotate(-tr);
+		c.translate(-this.dims.width/2,-this.dims.height/2);
+	},
+	sliceColour:function(p){
+		return 'rgb('+parseInt(p*255)+','+parseInt(136+(p*119))+',255)'
+	}
+});
